21ac848f1970fd336f9170cefc84850065c73ac4
Amy Gillies <2254790g@student.gla.ac.uk>
2020-03-30 19:38:14 +0100

    Chapter 7 - Exercises

--- rango/views.py
+++ rango/views.py
@@ -7,10 +7,12 @@ from rango.models import Category, Page
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
 
@@ -21,6 +23,7 @@ def about(request):
 
 def show_category(request, category_name_slug):
     context_dict = {}
+
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
@@ -72,9 +75,8 @@ def add_page(request, category_name_slug):
                 return redirect(reverse('rango:show_category',
                                         kwargs={'category_name_slug':
                                                 category_name_slug}))
-
             else:
                 print(form.errors)
 
-    context_dict = {'forms': form, 'category': category}
+    context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
